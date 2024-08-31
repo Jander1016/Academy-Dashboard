95653328bed4315475cd658da5ff7c42e0e9588b
Jander Gomez <jandergb.30gmail.com>
2024-09-01 00:24:58 +0200

    fix: create section Count Chart in admin

--- src/app/(ui)/(dashboard)/admin/page.tsx
+++ src/app/(ui)/(dashboard)/admin/page.tsx
@@ -1,20 +1,28 @@
+import { CountChart } from "@/components/CountChart"
 import UserCard from "@/components/UserCard"
 
 const AdminPage = () => {
   return (
-    <div className="p-4 flex gap-4 flex-col md:flex-row">
-      <div className="w-full lg:w-2/3">
-        <div className="flex gap-4 justify-between">
-          <UserCard type="student" />
-          <UserCard type="teacher" />
-          <UserCard type="parent" />
-          <UserCard type="staff" />
-        </div>
-      </div>
-      <div className="w-full lg:w-1/3">
-      </div>
+    <section className="p-4 flex gap-4 flex-col md:flex-row">
+      <section className="w-full lg:w-2/3 flex flex-col gap-8">
+        <article className="flex gap-4 justify-between flex-wrap">
+          <UserCard type="Estudiante" />
+          <UserCard type="Interesado" />
+          <UserCard type="Padres" />
+          <UserCard type="Profesores" />
+        </article>
+        <article className="flex gap-4 flex-col lg:flex-row">
+          <div className="w-full lg:w-1/3 h-[450px] bg-white">
+            <CountChart />
+          </div>
+          <div className="w-full lg:w-1/3 h-[450px]">
 
-    </div>
+          </div>
+        </article>
+      </section>
+      <section className="w-full lg:w-1/3">
+      </section>
+    </section>
   )
 }
 
